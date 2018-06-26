2c00e0262dc42bd056627384c4e621e989bfab0d
enoatu <acro1452@gmail.com>
2018-06-27 03:14:41 +0900

    pwaできない

--- cache/#home#enomoto#www#arrogancia2#app#views#components#header.volt.php
+++ cache/#home#enomoto#www#arrogancia2#app#views#components#header.volt.php
@@ -8,5 +8,13 @@
     <meta name="viewport" content="width=device-width">
     <?php $this->assets->outputCss(); ?>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="manifest" href="<?= $this->url->get('./manifest.json') ?>">
 </head>
 <body>
+ <script>
+    // service workerが有効なら、service-worker.js を登録します
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register("<?= $this->url->get('js/service-worker.js') ?>");
+    }   
+</script>
+
